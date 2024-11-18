6e876b2ab8a55e317fd9e35bd9e6c7523de607d7
nnazero <emily3472@gmail.com>
2024-11-18 14:38:24 +0900

    Feat: add EnterpriseSearch, connect KakaoMap API

--- src/pages/enterprise/EnterpriseSearch.jsx
+++ src/pages/enterprise/EnterpriseSearch.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from '../../styles/enterprise/EnterpriseSearch.module.css';
+import KakaoMap from '../../components/enterprise/KakaoMap';
+import searchIcon from '../../assets/images/enterprise/search-icon.svg';
 
 function EnterpriseSearch() {
 
     return (
         <div className={styles.container}>
             <p>기업찾기</p>
+            <button className={styles.searchBtn}>
+                <img src={searchIcon} alt={`searchIcon`} className={styles.searchIcon} />
+            </button>
+            <div className={styles.map}>
+                <KakaoMap />
+            </div>
         </div>
     );
 }

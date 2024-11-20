558d115a6fcb82cf2c3c28596b2481866ba76d19
nnazero <emily3472@gmail.com>
2024-11-20 15:00:07 +0900

    Feat: Mypage ReviewSlider 추가

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../../styles/mypage/Mypage.module.css';
 import TopBar from '../../components/layout/TopBar';
+import ReviewSlider from '../../components/mypage/ReviewSlider';
 
 function Mypage() {
     const [userData, setUserData] = useState(null);
@@ -50,7 +51,7 @@ function Mypage() {
                             <span className={styles.userAge}>{userData.age}세</span>
                         </div>
                         <div className={styles.userInfoRow2}>
-                            <span className={styles.reviewLabel}>리뷰</span>
+                            <span className={styles.reviewLabel}>나의 리뷰</span>
                             <span className={styles.reviewCount}>{userData.reviews.length}</span>
                         </div>
                     </div>
@@ -59,6 +60,18 @@ function Mypage() {
                     <button className={styles.profileManageBtn}>프로필관리</button>
                     <button className={styles.accountSettingBtn}>계정설정</button>
                 </div>
+            </div>
+            <div className={styles.reviewContainer}>
+                <div className={styles.reviewHeader}>
+                    <div className={styles.myReview}>
+                        <span>나의 리뷰</span>
+                        <span>{userData.reviews.length}</span>
+                    </div>
+                    <button className={styles.reviewBtn}>리뷰쓰기</button>
+                </div>
+                <ReviewSlider
+                    items={userData}
+                />
             </div>
         </div>
     );

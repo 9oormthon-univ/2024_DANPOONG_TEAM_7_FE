b0ca91b81ffadc407f47b958b03adcf3d5ff6736
YunJae00 <0109548@naver.com>
2024-11-22 16:07:30 +0900

    FEAT: 매거진 UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EnterpriseSearch from './pages/enterprise/EnterpriseSearch';
 import Magazine from './pages/magazine/Magazine';
+import MagazineDetail from './pages/magazine/MagazineDetail';
 import Home from './pages/home/Home';
 import Program from './pages/program/Program';
 import Mypage from './pages/mypage/Mypage';
@@ -30,6 +31,7 @@ function App() {
                 <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
             </Route>
             <Route element={<BackLayout/>}>
+              <Route path="/magazine/:id" element={<MagazineDetail />}/>
               <Route path='/mypage/review/write' element={<ReviewWrite/>}/> 
               <Route path='/mypage/review/editwrite' element={<EditWrite/>}/>  
               <Route path='/mypage/review' element={<Review/>}/> 
